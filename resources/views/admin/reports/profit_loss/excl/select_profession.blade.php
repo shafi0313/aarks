2f4eaf7b2c018d6d542c2a9ae67ec43f9ef6895e
shafi0313 <msh.shafiul@gmail.com>
2023-10-16 10:55:41 +0600

    Profit Loss GST Excl

--- resources/views/admin/reports/profit_loss/excl/select_profession.blade.php
+++ resources/views/admin/reports/profit_loss/excl/select_profession.blade.php
@@ -1,20 +1,19 @@
 @extends('admin.layout.master')
-@section('title','Profit & Loss GST Exclude')
+@section('title', 'Profit & Loss GST Exclude')
 @section('content')
-
-      <div class="main-content">
+    <div class="main-content">
         <div class="main-content-inner">
-          <div class="breadcrumbs ace-save-state" id="breadcrumbs">
-            <ul class="breadcrumb">
-              <li>
-                <i class="ace-icon fa fa-home home-icon"></i>
-                <a href="{{ route('admin.dashboard') }}">Home</a>
-              </li>
-              <li>Reports</li>
-              <li class="active">Profit & Loss GST Exclude</li>
-            </ul><!-- /.breadcrumb -->
+            <div class="breadcrumbs ace-save-state" id="breadcrumbs">
+                <ul class="breadcrumb">
+                    <li>
+                        <i class="ace-icon fa fa-home home-icon"></i>
+                        <a href="{{ route('admin.dashboard') }}">Home</a>
+                    </li>
+                    <li>Reports</li>
+                    <li class="active">Profit & Loss GST Exclude</li>
+                </ul><!-- /.breadcrumb -->
 
-            {{-- <div class="nav-search" id="nav-search">
+                {{-- <div class="nav-search" id="nav-search">
               <form class="form-search">
                 <span class="input-icon">
                   <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
@@ -22,79 +21,43 @@
                 </span>
               </form>
             </div><!-- /.nav-search --> --}}
-          </div>
-
-          <div class="page-content">
-
-
-            <div class="row">
-              <div class="col-xs-12">
-
-                <!-- PAGE CONTENT BEGINS -->
+            </div>
+            <div class="page-content">
                 <div class="row">
-                  <div class="col-xs-12">
+                    <div class="col-xs-12">
+                        <!-- PAGE CONTENT BEGINS -->
+                        <div class="row">
+                            <div class="col-xs-12">
 
-                    <div class="col-md-3">
-                    </div>
-                    <div class="col-md-6" style="padding:20px; border:1px solid #999999;">
-                      <form action="" method="">
-                        <div class="form-group">
-                          <label style="color:green;"> Activity Profit & Loss Exclude GST Report</label>
-                          <select class="form-control" id="proid" name="proid" tabindex="7" onchange="location = this.value;">
-                            <option value="">Select Business Activity Report</option>
-                            @foreach ($client->professions as $profession)
-                            <option value="{{ route('profit_loss_gst_excl.date',[$client->id,$profession->id]) }}"> {{$profession->name}} </option>
-                            @endforeach
-                        </select>
+                                <div class="col-md-3">
+                                </div>
+                                <div class="col-md-6" style="padding:20px; border:1px solid #999999;">
+                                    <form action="" method="">
+                                        <div class="form-group">
+                                            <label style="color:green;"> Activity Profit & Loss Exclude GST Report</label>
+                                            <select class="form-control" id="proid" name="proid" tabindex="7"
+                                                onchange="location = this.value;">
+                                                <option value="">Select Business Activity Report</option>
+                                                @foreach ($client->professions as $profession)
+                                                    <option
+                                                        value="{{ route('profit_loss_gst_excl.date', [$client->id, $profession->id]) }}">
+                                                        {{ $profession->name }} </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                    </form>
+
+                                </div>
+
+                            </div>
                         </div>
 
-                      </form>
-
-                    </div>
-
-                  </div>
-                </div>
-
-                <!-- PAGE CONTENT ENDS -->
-              </div><!-- /.col -->
-            </div><!-- /.row -->
-          </div><!-- /.page-content -->
+                        <!-- PAGE CONTENT ENDS -->
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.page-content -->
         </div>
-      </div><!-- /.main-content -->
+    </div><!-- /.main-content -->
 
-<!-- Script -->
-    <script>
-        jQuery(function($) {
-            var myTable =
-            $('#period-table')
-                .DataTable({
-                    bAutoWidth: false,
-                    "aoColumns": [
-                        {"bSortable": false},
-                        null, null, null, null, null,
-                        {"bSortable": false}
-                    ],
-                    "aaSorting": [],
-                    select: {
-                        style: 'multi'
-                    }
-                });
-             });
-        $(".datepicker").datepicker({
-            dateFormat: "dd/mm/yy",
-            changeMonth: true,
-            changeYear: true
-        });
-
-        $('#financial-year').keyup(function(){
-            let year = $(this).val();
-            if (year.length == 4){
-                $('#msg').show().html('Your selected financial year july '+(year-1) +' to June '+year);
-            }else{
-                $('#msg').hide();
-                //$('#msg').show().html('Must be 4 Digit');
-            }
-
-        });
-    </script>
 @stop
