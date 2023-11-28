dd7ee7657b1c7eefd1cccab4ff5c4683e099c01b
shafi0313 <msh.shafiul@gmail.com>
2023-11-28 13:46:42 +0600

    Add extra line breaks in print.blade.php

--- resources/views/admin/reports/complete_financial_report_t_f/print.blade.php
+++ resources/views/admin/reports/complete_financial_report_t_f/print.blade.php
@@ -68,7 +68,7 @@
                 <h1>{{ clientName($client) }}</h1>
                 <h4>ABN {{ $client->abn_number }}{{ $client->charitable_number?', Charitable License Number '.$client->charitable_number:'' }}
                     {{ $client->iran_number?', IRAN Number '.$client->iran_number:'' }}</h4>
-                    <br><br><br>
+                    <br><br><br><br><br><br>
                 <h4>FINANCIAL REPORT</h4>
                 <h3>Contents</h3>
                 <ul style="list-style: none;">
