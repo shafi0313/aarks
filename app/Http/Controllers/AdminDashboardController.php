138d12ba2b55e75892addf691ef401024bfcb4f5
shafi0313 <msh.shafiul@gmail.com>
2023-12-11 09:41:53 +0600

    Refactor date comparison in AdminDashboardController.php

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -27,11 +27,11 @@ class AdminDashboardController extends Controller
             $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
             foreach ($tables as $table) {
                 if (Schema::hasColumn($table, 'client_id') && Schema::hasColumn($table, 'deleted_at')) {
-                    DB::table($table)->where('client_id', $client)->where('deleted_at', '2023-12-11')->update(['deleted_at' => null]);
+                    DB::table($table)->where('client_id', $client)->whereDate('deleted_at', '2023-12-11')->update(['deleted_at' => null]);
                 }
             }
 
-            Client::where('id', $client)->where('deleted_at', '2023-12-11')->update(['deleted_at' => null]);
+            Client::where('id', $client)->whereDate('deleted_at', '2023-12-11')->update(['deleted_at' => null]);
         }
         // do {
         //     DB::table('trxid')->insert(['trxid' => transaction_id('TRX')]);
